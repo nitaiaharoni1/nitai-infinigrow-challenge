9ec727d2abdd4371f0fb0ceff627e7ad675331d6
nitaiaharoni <nitaiaharoni1@gmail.com>
2022-05-24 16:24:32 +0300

    init

--- src/pages/BudgetPlanPage/components/BudgetPlanTab1/components/BudgetPlanTab1Row/BudgetPlanTab1Row.tsx
+++ src/pages/BudgetPlanPage/components/BudgetPlanTab1/components/BudgetPlanTab1Row/BudgetPlanTab1Row.tsx
@@ -1,21 +1,35 @@
 import { FC } from 'react';
 
-import { AffiliateProgram, ArrowDown } from 'assets';
+import { AffiliateProgram, ArrowDown, Dots } from 'assets';
 
 interface Props {
   onClick: () => void;
   isExpanded: boolean;
 }
 
-export const BudgetPlanTab1Row: FC<Props> = ({ onClick, isExpanded }) => (
-  <div className='cursor-pointer w-full	border-2 h-14 flex items-center' onClick={onClick}>
-    <ArrowDown className={`ml-4 ${isExpanded && 'rotate-180'}`}/>
-
-    <AffiliateProgram className='ml-8'/>
-
-    <div className='ml-4'>
-      Paid reviews
+export const BudgetPlanTab1Row: FC<Props> = ({
+  onClick,
+  isExpanded
+}) => {
+  
+  const handleDotsClick = (e: any) => {
+    e.stopPropagation();
+    console.log('handleDotsClick');
+  };
+  
+  return (
+    <div className='flex w-full	border-2 h-14 items-center justify-between px-4' onClick={onClick}>
+      <div className='cursor-pointer flex items-center'>
+        <ArrowDown className={`${isExpanded && 'rotate-180'}`}/>
+  
+        <AffiliateProgram className='ml-8'/>
+    
+        <div className='ml-4'>
+          Paid reviews
+        </div>
+      </div>
+      <Dots onClick={handleDotsClick} className='cursor-pointer'/>
     </div>
-  </div>
-);
+  );
+};
 
--- src/pages/BudgetPlanPage/components/BudgetPlanTab1/components/BudgetPlanTab1RowDetails/BudgetPlanTab1RowDetails.tsx
+++ src/pages/BudgetPlanPage/components/BudgetPlanTab1/components/BudgetPlanTab1RowDetails/BudgetPlanTab1RowDetails.tsx
@@ -6,21 +6,48 @@ interface Props {
 }
 
 export const BudgetPlanTab1RowDetails: FC<Props> = ({}) => (
-  <div className={'border-2 p-8'} style={{ height: 580 }}>
+  <div className={'border-2 p-8 flex space-x-24'} style={{ height: 580, marginTop: -2 }}>
+    {/* Budget Frequency */}
     <div>
-      <div>
+      <div className={'flex items-center space-x-2 text-gray-500'}>
         <div>
         Budget Frequency
         </div>
-        <Info></Info>
+        <Info/>
       </div>
       {/*dropdown*/}
       <div>
 
       </div>
     </div>
+    
+    
+    {/* Baseline [Annual] Budget */}
     <div>
+      <div className={'flex items-center space-x-2 text-gray-500'}>
+        <div>
+        Baseline [Annual] Budget
+        </div>
+        <Info/>
+      </div>
+      {/*dropdown*/}
+      <div>
 
+      </div>
+    </div>
+    
+    {/* Budget Allocation */}
+    <div>
+      <div className={'flex items-center space-x-2 text-gray-500'}>
+        <div>
+        Budget Allocation
+        </div>
+        <Info/>
+      </div>
+      {/*dropdown*/}
+      <div>
+
+      </div>
     </div>
   </div>
 );
